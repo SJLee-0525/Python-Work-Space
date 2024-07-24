2f524644d9adb9c3ceffe8428407599baf4887b2
sungjoon111 <sungjoon25@yonsei.ac.kr>
2024-07-24 17:25:27 +0900

    240724-s01

--- baekjoon/baekjoon_2108.py
+++ baekjoon/baekjoon_2108.py
@@ -1,18 +1,43 @@
-from sys import *
+import sys
 
-n = int(stdin.readline())
+n = int(sys.stdin.readline())
 
-l = []
+num_dict = {}
 for _ in range(n):
-    l.append(int(stdin.readline()))
+    temp = int(sys.stdin.readline())
+    if temp not in num_dict:
+        num_dict[temp] = 1
+    else:
+        num_dict[temp] += 1
+    
+# print(num_dict) # {1: 1, 3: 1, 8: 1, -2: 1, 2: 1}
 
-l.sort()
+num_sum = 0
+num_list = [] 
+value_key_dict = {}
 
-b = []
-for i in range(1, n):
-    if l[i] == l[i - 1]:
+for key, value in num_dict.items():
+    num_sum += key * value
+    num_list.extend([key] * value)
+    if value not in value_key_dict:
+        value_key_dict[value] = [key]
+    else:
+        value_key_dict[value].extend([key])
 
-print(int(sum(l)/n))
-print(l[(n - 1) / 2])
-print()
-print(max(l) - min(l))
+# print(value_key_dict) # {1: [1, 3, 8, -2, 2]}
+sorted_value_key = sorted(value_key_dict)
+sorted_max_value = sorted_value_key.pop()
+
+if len(value_key_dict[sorted_max_value]) == 1:
+    frequent_value = value_key_dict[sorted_max_value][0]
+else:
+    frequent_value = sorted(value_key_dict[sorted_max_value])[1]
+    # print(frequent_value)
+
+num_list.sort()
+
+# print('---')
+print(round(num_sum / n)) # 평균
+print(num_list[int((n - 1) / 2)]) # 중앙
+print(frequent_value)
+print(num_list[-1] - num_list[0]) # 차이
