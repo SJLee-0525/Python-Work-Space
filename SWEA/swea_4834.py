48c0991b9a044b01ae5fa13e8b62d53952ca5c43
sungjoon111 <sungjoon25@yonsei.ac.kr>
2024-07-30 17:40:00 +0900

    240730-s01

--- SWEA/swea_4834.py
+++ SWEA/swea_4834.py
@@ -1,34 +1,27 @@
 T = int(input())
 
-for test in range(1, T + 1):
-    N = int(input())
-    card_list = list(map(int, list(input())))
+for test_case in range(1, T + 1):
+    N = int(input()) # 카드 장수
+    card_list = list(map(int, input()))
 
-    card_dict = {}
-    for target in card_list:
-        count = 0
-        for card in card_list:
-            if target == card:
-                count += 1
-    
-        if count not in card_dict:
-            card_dict[count] = [target]
-        else:
-            if target not in card_dict[count]:
-                card_dict[count].append(target)
-    
-    card_counts_key = list(card_dict.keys())
-    
-    max_key = card_counts_key[0]
-    for card_key in card_counts_key:
-        if max_key < card_key:
-            max_key = card_key
-    
-    final_value_list = card_dict[max_key]
+    # 카드 리스트에서 최대 값 찾기
+    max_value = card_list[0]
+    for value in card_list:
+        if max_value < value:
+            max_value = value
 
-    max_value = final_value_list[0]
-    for final_value in final_value_list:
-        if max_value < final_value:
-            max_value = final_value
-    
-    print(f'#{test} {max_value} {max_key}')
+    # 카드 번호를 인덱스로, 개수를 값으로 하는 리스트 생성
+    counts = [0] * (max_value + 1)
+    for card_num in card_list:
+        counts[card_num] += 1
+
+    # counts 리스트 내에서 최대 값 찾기
+    # 만약 값이 크거나 같다면 인덱스 번호를 이용해 카드 번호 갱신
+    max_i = counts[0]
+    max_card = 0
+    for card_num, i in enumerate(counts):
+        if max_i <= i:
+            max_i = i
+            max_card = card_num
+
+    print(f'#{test_case} {max_card} {max_i}')
