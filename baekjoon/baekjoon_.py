bbf0933e46acd7d43d05d61079030d3cf1ddf04c
sungjoon111 <sungjoon25@yonsei.ac.kr>
2024-08-07 17:46:09 +0900

    240807-s01

--- baekjoon/baekjoon_.py
+++ baekjoon/baekjoon_.py
@@ -1,9 +1,20 @@
 import sys
 
-for _ in range(4):
-    x1, y1, p1, q1, x2, y2, p2, q2 = map(int, sys.stdin.readline().split())
-    square_1 = [(x1, q1), (p1, q1), (p1, y1), (x1, y1)] # 1 2
-    square_2 = [(x2, q2), (p2, q2), (p2, y2), (x2, y2)] # 4 3
+C = int(sys.stdin.readline())
 
-    if x1 in range(x2, p2 + 1) or p1 in range(x2, p2 + 1) or y1 in range(y2, q2 + 1) or q1 in range(y2, q2 + 1):
-        if (x1 == q2 and (y2 in range(y1, q2 + 1) or q2 in range(y1, q2 + 1))) or (q1 == x2 and
+stair = [0] + [int(sys.stdin.readline()) for _ in range(C)]
+
+i = 0
+score = 0
+while i <= C - 2:
+    if stair[i + 1] >= stair[i + 2]:
+        score += stair[i + 1]
+        i += 1
+    elif stair[i + 1] < stair[i + 2]:
+        score += stair[i + 2]
+        i += 2
+
+if i == C - 1:
+    score += stair[i + 1]
+
+print(score)
