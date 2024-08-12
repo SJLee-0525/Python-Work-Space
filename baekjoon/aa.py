3d5af633b0d2f5bcb4a894d48aacf25daa40ddc2
sungjoon111 <sungjoon25@yonsei.ac.kr>
2024-08-12 17:55:34 +0900

    240812-s01

--- baekjoon/aa.py
+++ baekjoon/aa.py
@@ -1,22 +1,15 @@
-def f(i, V, C): # V개의 집합에서 i 원소의 포함여부 결정
-    if i == V: # 모든 원소에 대해 결정하면
-        for i in range(V):
-            c = 0
-            if b[i]:
-                c += 1
-                print(arr[i], end=' ')
-        print()
-    else:
-        b[i] = 1        # a[i] 원소가 부분집합에 포함
-        f(i + 1, V)     # 다음 원소를 찾으러 가봐
-        b[i] = 0        # a[i] 원소가 부분집합에 포함되지 않음
-        f(i + 1, V)
+import sys
 
-T = int(input())
-for tc in range(1, T + 1):
-    N, K = map(int, input().split()) # N 원소의 수 K 집합의 합
-    arr = list(range(1, 7))
-    b = [0] * 6 
+N = int(sys.stdin.readline())
 
-    # 재ㅔ귀로 모든 부분집합 만들기
-    f(0, 6, 5) 
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+di = {}
+for i in range(N):
+    for j in range(i, N):
+        if i != j:
+            di[(i, j)] = arr[i][j] + arr[j][i]
+print(di)
+
+for key, value in di.items():
+    
