e2a9d3aa525a2d11f327a9bd1823df8d40961857
sungjoon111 <sungjoon25@yonsei.ac.kr>
2024-08-22 15:39:32 +0900

    240822-s01

--- baekjoon/baekjoon__.py
+++ baekjoon/baekjoon__.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 # def combi(i, K):
 #     if i == K:
@@ -15,33 +16,62 @@ import sys
 #         B[i] = 0
 #         combi(i + 1, K)
 
-def combi_2(K):
-    new_map = [new[:] for new in room]
-    for i in range(K - 2):
-        for j in range(i, K - 1):
-            if i != j:
-                for k in range(j, K):
-                    if i != j and j != K:
-                        new_map[empty_list[i][0]][empty_list[i][1]] = 1
-                        new_map[empty_list[j][0]][empty_list[j][1]] = 1
-                        new_map[empty_list[k][0]][empty_list[k][1]] = 1
-                        bfs(new_map)
+# def combi_2(K):
+#     for i in range(K - 2):
+#         for j in range(i, K - 1):
+#             if i != j:
+#                 for k in range(j, K):
+#                     if i != j and j != K:
+#                         new_map = [new[:] for new in room]
+#                         new_map[empty_list[i][0]][empty_list[i][1]] = 1
+#                         new_map[empty_list[j][0]][empty_list[j][1]] = 1
+#                         new_map[empty_list[k][0]][empty_list[k][1]] = 1
+#                         bfs(new_map)
+#                         # print(new_map)
+
+# def combi_3(K):
+#     for i in range(1<<K):
+#         temp = []
+#         for j in range(K):
+#             if i & (1<<j):
+#                 temp.append(empty_list[j])
+#         if len(temp) == 3:
+#             new_map = [new[:] for new in room]
+#             new_map[temp[0][0]][temp[0][1]] = 1
+#             new_map[temp[1][0]][temp[1][1]] = 1
+#             new_map[temp[2][0]][temp[2][1]] = 1
+#             bfs(new_map)
+
+def combi_4(K):
+    bit_N = 2**K - 1
+
+    for i in range(bit_N):
+        if bin(i)[2:].count('1') == 3:
+            new_map = [new[:] for new in room]
+            for j in range(5):
+                if i & (1<<j) != 0 :
+                    new_map[empty_list[4-j][0]][empty_list[4-j][1]] = 1
+            bfs(new_map)
+
 
 def bfs(input_map):
     global cnt
+    Q = deque()
     for vi, vj in virus_list:
-        for k in range(4):
-            mi, mj = vi + di[k], vj + dj[k]
-            if 0 <= mi < N and 0 <= mj < M and input_map[mi][mj] == 0:
-                while 0 <= mi < N and 0 <= mj < M and input_map[mi][mj] == 0:
-                    input_map[mi][mj] = 2
-                    mi += di[k]
-                    mj += dj[k]
+        Q.append((vi, vj))
 
+    while Q:
+        i, j = Q.popleft()
+        input_map[i][j] = 2
+        for k in range(4):
+            mi, mj = i + di[k], j + dj[k]
+            if 0 <= mi < N and 0 <= mj < M and input_map[mi][mj] == 0 and (mi, mj) not in Q:
+                Q.append((mi, mj))
+    
     c = 0
-    for i in range(N):
-        for j in range(M):
-            if input_map[i][j] == 0:
+    for ii in range(N):
+        for jj in range(M):
+            if input_map[ii][jj] == 0:
                 c += 1
     if cnt < c:
         cnt = c
@@ -66,8 +96,6 @@ dj = [0, 1, 0, -1]
 
 K = len(empty_list)
 cnt = 0
-# B = [0] * K
-# combi(0, K)
-combi_2(K)
+combi_4(K)
 
 print(cnt)
