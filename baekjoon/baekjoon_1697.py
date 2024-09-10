4f5f04f7dde351d6ab4fcdd2e4cb5b5bdcac64dd
sungjoon111 <sungjoon25@yonsei.ac.kr>
2024-09-10 17:19:06 +0900

    240910-s01

--- baekjoon/baekjoon_1697.py
+++ baekjoon/baekjoon_1697.py
@@ -1,24 +1,26 @@
 import sys
+from collections import deque
 
-def find_D(S, i_D, c):
-    global count
-    if i_D == S:
-        if count > c:
-            count = c
-        return
-    if c >= count:
-        return
-    if S > i_D:
-        find_D(S, i_D + 1, c + 1)
-    else:
-        find_D(S, i_D + 1, c + 1)
-        find_D(S, i_D - 1, c + 1)
-        find_D(S, i_D // 2, c + 1)
+def fnd(N, K):
+    Q = deque()
+    Q.append(N)
+    visited = [0] * 100001
+    visited[N] = 1
+    while Q:
+        n = Q.popleft()
+        if n == K:
+            return visited[n] - 1
+        if 0 <= n - 1 and visited[n - 1] == 0:
+            Q.append(n - 1)
+            visited[n - 1] = visited[n] + 1
+        if n + 1 <= 100000 and visited[n + 1] == 0:
+            Q.append(n + 1)
+            visited[n + 1] = visited[n] + 1
+        if n * 2 <= 100000 and visited[n * 2] == 0:
+            Q.append(n * 2)
+            visited[n * 2] = visited[n] + 1
 
+###########################################################
 
-###################################################
-
-S, D = map(int, sys.stdin.readline().split())
-count = 1000000
-find_D(S, D, 0)
-print(count)
+N, K = map(int, sys.stdin.readline().split())
+print(fnd(N, K))
