418c6e9f67d0ddb69d77a324e6f4440b5cc275f6
sjlee-0525 <sungjoon25@yonsei.ac.kr>
2024-10-24 18:29:16 +0900

    241024-s02

--- baekjoon/baekjoon_14938.py
+++ baekjoon/baekjoon_14938.py
@@ -4,28 +4,30 @@ import heapq
 def dijkstra(start):
     global result
 
-    dist = [10000001] * (N + 1)
-    items = [0] * (N + 1)
-    items[start] = itemList[start]
+    dist = [10000001] * (N + 1) # 초기 거리 설정
+    dist[start] = 0  # 시작 지점의 거리는 0
+
     priorityQueue = []
-    heapq.heappush(priorityQueue, (0, items[start], start))
-    itemCnt = itemList[start]
+    heapq.heappush(priorityQueue, (0, start)) # 거리 지점
 
     while priorityQueue:
-        curr_dist, curr_item, curr_place = heapq.heappop(priorityQueue)
+        curr_dist, curr_place = heapq.heappop(priorityQueue)
+        if curr_dist > dist[curr_place]:
+            continue  # 이미 처리된 지점은 건너뜀
+
         for next_info in adjL[curr_place]:
             next_place, next_dist = next_info
             new_dist = curr_dist + next_dist
-            new_item = curr_item + itemList[next_place]
-            if new_dist <= M and new_dist < dist[next_place] and new_item >= items[next_place]:
-                items[next_place] = new_item
-                dist[next_place] = new_dist
-                itemCnt += itemList[next_place]
-                heapq.heappush(priorityQueue, (new_dist, new_item, next_place))
 
-    # print(items)
-    if result < itemCnt:
-        result = itemCnt
+            # 새로운 거리가 M 이하면서, 더 짧으면
+            if new_dist <= M and new_dist < dist[next_place]:
+                dist[next_place] = new_dist
+                heapq.heappush(priorityQueue, (new_dist, next_place))
+
+    # 도달 가능한 지역들의 아이템을 추가
+    itemCnt = sum(itemList[i] for i in range(1, N + 1) if dist[i] <= M)
+
+    result = max(result, itemCnt)
 
 #################################################################################
 
@@ -40,6 +42,7 @@ for _ in range(R):
 
 result = 0
 
+# 모든 시작 지점에서 다익스트라 탐색 수행
 for start in range(1, N + 1):
     dijkstra(start)
 
