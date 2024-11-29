9235d4d32a894a1018d5a7c47af82b06f0dbd89f
sungjoon111 <sungjoon25@yonsei.ac.kr>
2024-11-30 01:44:02 +0900

    241130-h01

--- baekjoon/baekjoon_9252.py
+++ baekjoon/baekjoon_9252.py
@@ -1,33 +1,24 @@
-import sys
-#import pprint
+import sys, pprint
 
 strA = sys.stdin.readline().rstrip()
 strB = sys.stdin.readline().rstrip()
 
-LCS = [[0] * (len(strB) + 1) for  _ in range(len(strA) + 1)]
+LCS = [[''] * (len(strB) + 1) for _ in range(len(strA) + 1)]
 
-for a in range(len(strA)):
-    for b in range(len(strB)):
-        if strA[a] == strB[b]:
-            LCS[a + 1][b + 1] = LCS[a][b + 1] + 1
+for a in range(1, len(strA) + 1):
+    for b in range(1, len(strB) + 1):
+        if strA[a - 1] == strB[b - 1]:
+            LCS[a][b] = LCS[a - 1][b - 1] + strA[a - 1]
         else:
-            LCS[a + 1][b + 1] = max(LCS[a][b + 1], LCS[a + 1][b])
+            if len(LCS[a - 1][b]) >= len(LCS[a][b - 1]):
+                LCS[a][b] = LCS[a - 1][b]
+            else:
+                LCS[a][b] = LCS[a][b - 1]
 
 # pprint.pprint(LCS)
 
-stack = []
-i, j = len(strA), len(strB)
+result = LCS[-1][-1]
+print(len(result))
 
-while i > 0 and j > 0 and LCS[i][j] > 0:
-    if LCS[i - 1][j] == LCS[i][j - 1] == LCS[i][j] - 1:
-        stack.append(strB[j - 1])
-        i -= 1
-        j -= 1
-    elif LCS[i - 1][j] == LCS[i][j]:
-        i -= 1
-    elif LCS[i][j - 1] == LCS[i][j]:
-        j -= 1
-
-print(LCS[len(strA)][len(strB)])
-if LCS[len(strA)][len(strB)] > 0:
-    print(''.join(stack[::-1]))
+if len(result) != 0:
+    print(result)
